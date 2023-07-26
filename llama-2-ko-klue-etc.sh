1215bcbb13c8b939e91ef7215f67214e2f4a8709
junbum lee <jun@beomi.net>
2023-07-26 00:34:47 +0000

    rename scripts & add bench for 40B ckpt

--- llama-2-ko-klue-etc.sh
+++ llama-2-ko-klue-etc.sh
@@ -4,14 +4,16 @@ RESULT_DIR='results/korean_origin_bench'
 TASKS='klue_nli,klue_sts,klue_ynat,kohatespeech,kohatespeech_apeach,kohatespeech_gen_bias,korunsmile,nsmc,pawsx_ko'
 GPU_NO=3
 
-CURRENT_TRAINED_TOKENS=20b
+CURRENT_TRAINED_TOKENS=40b
 
 echo "mkdir -p $RESULT_DIR/$CURRENT_TRAINED_TOKENS"
 mkdir -p $RESULT_DIR/$CURRENT_TRAINED_TOKENS
 
+MODEL='beomi/llama-2-ko-7b'
+
 python main.py \
 --model gpt2 \
---model_args pretrained=../EasyLM/llama-2-ko-7b \
+--model_args pretrained=$MODEL \
 --tasks $TASKS \
 --num_fewshot 0 \
 --device cuda:$GPU_NO \
@@ -19,7 +21,7 @@ python main.py \
 
 python main.py \
 --model gpt2 \
---model_args pretrained=../EasyLM/llama-2-ko-7b \
+--model_args pretrained=$MODEL \
 --tasks $TASKS \
 --num_fewshot 5 \
 --device cuda:$GPU_NO \
@@ -27,16 +29,16 @@ python main.py \
 
 python main.py \
 --model gpt2 \
---model_args pretrained=../EasyLM/llama-2-ko-7b \
+--model_args pretrained=$MODEL \
 --tasks $TASKS \
 --num_fewshot 10 \
 --device cuda:$GPU_NO \
 --output_path $RESULT_DIR/$CURRENT_TRAINED_TOKENS/10_shot.json
 
-python main.py \
---model gpt2 \
---model_args pretrained=../EasyLM/llama-2-ko-7b \
---tasks $TASKS \
---num_fewshot 50 \
---device cuda:$GPU_NO \
---output_path $RESULT_DIR/$CURRENT_TRAINED_TOKENS/50_shot.json
+#python main.py \
+#--model gpt2 \
+#--model_args pretrained=$MODEL \
+#--tasks $TASKS \
+#--num_fewshot 50 \
+#--device cuda:$GPU_NO \
+#--output_path $RESULT_DIR/$CURRENT_TRAINED_TOKENS/50_shot.json
